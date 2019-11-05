969cd805788b83e43292a74b91b9ca4f193fea0f
Mikhail <nikolaev.ma@phystech.edu>
2019-11-05 18:31:48 +0300

    change

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from RegularMachine import RegularMachine
 from LongestSuffix import longest_suffix
 
 if __name__ == '__main__':
-    while(True):
-        print("Введите через Enter две строки: регулярное выражение и слово u.\n"
-              "Для выхода введите Exit")
-        regular_machine_str = input()
-        if regular_machine_str == "Exit":
-            exit()
-        pattern = input()
-        result = longest_suffix(regular_machine_str, pattern)
-        print(result)
+    input_str = input()
+    two_str = input_str.split()
+    if len(two_str) != 2:
+        raise Exception
+    regular_machine_str = two_str[0]
+    pattern = two_str[1]
+    print(longest_suffix(regular_machine_str, pattern))
+
+
